a6288a490b91f657941f0f0fff740a9f367713eb
aoisupersix <aoisupersix@gmail.com>
2018-06-27 22:48:50 +0900

    [add] 距離程の表示機能を実装

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,97 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "bve5-language-suport" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    let distChecker = new DistanceChecker();
+    let controller = new DistanceCheckerController(distChecker);
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(controller);
+    context.subscriptions.push(distChecker);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+class DistanceCheckerController {
+
+    private _distChecker: DistanceChecker;
+    private _disposable: vscode.Disposable;
+
+    constructor(distChecker: DistanceChecker) {
+        this._distChecker = distChecker;
+
+        // subscribe to selection change and editor activation events
+        let subscriptions: vscode.Disposable[] = [];
+        vscode.window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
+        vscode.window.onDidChangeActiveTextEditor(this._onEvent, this, subscriptions);
+
+        // update the counter for the current file
+        this._distChecker.updateDistance();
+
+        // create a combined disposable from both event subscriptions
+        this._disposable = vscode.Disposable.from(...subscriptions);
+    }
+
+    dispose() {
+        this._disposable.dispose();
+    }
+
+    private _onEvent() {
+        this._distChecker.updateDistance();
+    }
+}
+
+class DistanceChecker {
+
+    private _statusBarItem: vscode.StatusBarItem =  vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+
+    public updateDistance() {
+
+        // テキストエディタを取得
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            this._statusBarItem.hide();
+            return;
+        }
+
+        let doc = editor.document;
+        if (doc.languageId === "bve-map-2.02") {
+            let distString = this._getDistance(doc, editor.selections);
+
+            this._statusBarItem.text = `距離程： ${distString} [m]`;
+            this._statusBarItem.show();
+        } else {
+            this._statusBarItem.hide();
+        }
+    }
+
+    /**
+     * 現在のカーソル位置の距離程を取得します。
+     * @param doc TextDocument
+     * @param selections Cursors
+     */
+    public _getDistance(doc: vscode.TextDocument, selections: vscode.Selection[]): string {
+
+        if (selections.length === 1) {
+            let pos = selections[0].active;
+            let range = new vscode.Range(new vscode.Position(0,0), pos);
+            let utext = doc.getText(range);
+
+            //距離程がカーソルの位置より上にあるか
+            let m = utext.match(/\d+(?:\.\d+)?;/gi);
+            if (m !== null){
+                return m[m.length - 1].replace(';', '');
+            }
+        }
+
+        //距離程なし
+        return "---";
+    }
+
+    dispose() {
+        this._statusBarItem.dispose();
+    }
 }
