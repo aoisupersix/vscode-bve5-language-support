e5c197aafe9ce9c938c6da386936f2f4ae73d796
aoisupersix <aoisupersix@gmail.com>
2018-10-28 17:09:05 +0900

    [update] MapCompletionItemの生成をメソッド分割

--- src/bve-map/MapCompletionItemProvider.ts
+++ src/bve-map/MapCompletionItemProvider.ts
@@ -8,6 +8,9 @@ import { List } from 'linqts';
 
 export class MapCompletionItemProvider implements vscode.CompletionItemProvider {
 
+    public static readonly FUNC_COMPLETION_TOKEN: string = '.';
+    public static readonly KEY_COMPLETION_TOKEN: string = '\'';
+
     /**
      * 引数に与えられた文字列から関数名を取得して返します。
      * @param trimedText 整形済みのマップファイルテキスト
@@ -21,6 +24,54 @@ export class MapCompletionItemProvider implements vscode.CompletionItemProvider 
         return trimedText.substring(startIdx, endIdx);
     }
 
+    /**
+     * 関数名のCompletionItemを生成して返します。
+     * @param txt マップ構文
+     */
+    private getFuncCompletionItems(txt: string): Promise<vscode.CompletionItem[]> {
+
+        return new Promise((resolve, reject) => {
+            let mapElementName = this.getFuncName(txt);
+            let syntaxes = MapDocs.instance.getSyntaxes();
+            let ret = new List<vscode.CompletionItem>();
+
+            if(mapElementName === "") { reject(); }
+            else {
+                //一致する補完の追加
+                for(let i in syntaxes) {
+                    //関数名補完
+                    if(syntaxes[i].isMatchMapElement(mapElementName)) {
+                        let item = syntaxes[i].getFunctionCompletionItem();
+                        //重複チェック
+                        if(!ret.Any(x => x!.label === item.label)) {
+                            ret.Add(item);
+                        }
+                    }
+                    if(syntaxes[i].getSyntaxType() === MapSyntaxType.Syntax3) {
+                        //シンタックス3のマップ要素2補完
+                        if(syntaxes[i].isMatchMapElement1(mapElementName)) {
+                            let item = syntaxes[i].getMapElement2CompletionItem();
+                            //重複チェック
+                            if(!ret.Any(x => x!.label === item.label)) {
+                                ret.Add(item);
+                            }                         
+                        }
+                    }
+                }
+            }
+
+            if(ret.Any()) { resolve(ret.ToArray()); }
+            
+            reject();
+        });
+    }
+
+    private getKeyCompletionItems(txt: string): Promise<vscode.CompletionItem[]> {
+        return new Promise((resolve, reject) => {
+
+        });
+    }
+
     public provideCompletionItems(
         document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken):
         Promise<vscode.CompletionItem[]> {
@@ -29,44 +80,11 @@ export class MapCompletionItemProvider implements vscode.CompletionItemProvider 
             let txt = util.trimMapText(
                 document.getText(new vscode.Range(new vscode.Position(0, 0), position))
             );
-            //let nowChar = txt.substring(txt.length - 1);
-            let mapElementName = this.getFuncName(txt);
+            let nowChar = txt.substring(txt.length - 1);
 
-            return new Promise((resolve, reject) => {
-                let syntaxes = MapDocs.instance.getSyntaxes();
-                let ret = new List<vscode.CompletionItem>();
+            if (nowChar === MapCompletionItemProvider.FUNC_COMPLETION_TOKEN) { return this.getFuncCompletionItems(txt); }
+            else if (nowChar === MapCompletionItemProvider.KEY_COMPLETION_TOKEN) { return this.getKeyCompletionItems(txt); }
 
-                if(mapElementName === "") {
-                    reject();
-                }else {
-
-                    //一致する補完の追加
-                    for(let i in syntaxes) {
-                        //関数名補完
-                        if(syntaxes[i].isMatchMapElement(mapElementName)) {
-                            let item = syntaxes[i].getFunctionCompletionItem();
-                            //重複チェック
-                            if(!ret.Any(x => x!.label === item.label)) {
-                                ret.Add(item);
-                            }
-                        }
-                        if(syntaxes[i].getSyntaxType() === MapSyntaxType.Syntax3) {
-                            //シンタックス3のマップ要素2補完
-                            if(syntaxes[i].isMatchMapElement1(mapElementName)) {
-                                let item = syntaxes[i].getMapElement2CompletionItem();
-                                //重複チェック
-                                if(!ret.Any(x => x!.label === item.label)) {
-                                    ret.Add(item);
-                                }                         
-                            }
-                        }
-                    }
-                }
-
-                if(ret.Any()) {
-                    resolve(ret.ToArray());
-                }
-                reject();
-            });
+            return new Promise((resolve, reject) => { reject(); });
     }
 }
