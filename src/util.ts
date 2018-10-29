e6f22edb69408f9aff71676e0282de8096bb8dd2
aoisupersix <aoisupersix@gmail.com>
2018-10-29 16:51:40 +0900

    [Refactor] 2重になっていた処理を削除

--- src/util.ts
+++ src/util.ts
@@ -3,6 +3,7 @@
 import * as fs from 'fs'
 import * as encoding from 'encoding-japanese'
 import * as iconv from 'iconv-lite'
+import * as headers from './const/headers';
 
 /**
  * 行コメントを示すデフォルトの正規表現パターンです。
@@ -14,21 +15,7 @@ export const COMMENT_REGEX: RegExp = /#|\/\//
  * @param mapTextData 未整形のマップファイルテキスト
  */
 export function trimMapText(mapTextData: string): string {
-  //ヘッダの削除
-  let headerRegex = /^\s*BveTs\s*Map\s*\d+\.\d+\s*(?::.*)?\s*(?:$|\r\n|\n|\r)/gi
-  mapTextData = mapTextData.replace(headerRegex, '')
-
-  //WhiteSpaceの削除
-  let lines = mapTextData.split('\n')
-  var ret = ''
-  for (let i in lines) {
-    let commentIdx = lines[i].search(/#|\/\//)
-    if (commentIdx !== -1) {
-      lines[i] = lines[i].substring(0, commentIdx)
-    }
-    ret += lines[i].replace(/\s+/g, '')
-  }
-  return ret
+  return trimWhiteSpace(mapTextData, headers.MAP_HEADER, COMMENT_REGEX, true);
 }
 
 /**
